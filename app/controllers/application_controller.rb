339d4e7a86d5d26540ff9466466a00807fba3cc4
shunsuke.kanari <kanari01git@gmail.com>
2018-06-05 05:09:38 +0000

    add function Favorite

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,18 +11,10 @@ class ApplicationController < ActionController::Base
     end
   end
   
-  def require_user_logged_in
-    unless logged_in?
-      redirect_to login_url
-    end
-  end
-
   def counts(user)
     @count_microposts = user.microposts.count
     @count_followings = user.followings.count
     @count_followers = user.followers.count
+    @count_likes = user.likes.count
   end
-  
-  
-  
 end
--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -3,4 +3,8 @@ class Micropost < ApplicationRecord
   
   validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 255 }
+  
+  has_many :reverses_of_favorite, class_name: 'Favorite', foreign_key: 'micropost_id'
+  has_many :likers, through: :reverses_of_favorite, source: :user
+
 end
